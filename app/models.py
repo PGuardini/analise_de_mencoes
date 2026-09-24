60ceb96ea6dc4be1fcf0d4c5f37785c49fd7defc
Pedro Guardini <pgabiattiguardini@gmail.com>
2026-09-24 12:39:45 -0300

    Criadas as models Brand, Mention e Response

---
+++ app/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from sqlmodel import Field, SQLModel, Relationship
+
+class Response(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    question: str
+    plataform: str
+    model: str
+    response_text: str
+    date_hour: datetime
+    sentiment: str | None = None
+
+    # Response-Mention Relationship 1-N
+    mentions: list['Mention'] = Relationship(back_populates='response')
+
+
+class Brand(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+
+    # Brand-Mention Relationship 1-N
+    mentions: list['Mention'] = Relationship(back_populates='brand')
+
+
+class Mention(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    # Mention-Response Relationship 1-1
+    id_response: int | None = Field(default=None, foreign_key='response.id')
+    response: Response | None = Relationship(back_populates='mentions')
+
+    # Mention-Brand Relationship 1-1
+    id_brand: int | None = Field(default=None, foreign_key='brand.id')
+    brand: Brand | None = Relationship(back_populates='mentions')
