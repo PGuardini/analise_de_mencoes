951aef7cdc0a5b8a4a909b85a6a7fef4026711a1
Pedro Guardini <pgabiattiguardini@gmail.com>
2026-09-23 22:18:43 -0300

    Funções para tratamento de dados criadas em data_manager/services.py

---
+++ data_manager/services.py
@@ -0,0 +1,80 @@
+from datetime import datetime
+import json
+import re
+from dateutil import parser
+
+JSON_PATH = 'respostas-exemplo.json'
+
+def json_consumer(json_path):
+    raw_data = None
+    with open(json_path, 'r', encoding='UTF-8') as f:
+        raw_data = json.load(f)
+
+    return raw_data
+
+
+
+def data_cleansing(raw_data: list[dict] | dict):
+    """Data ingestion first step before database persistency"""
+
+    date_pattern = '%d/%m/%Y %H:%M:%S'
+    plataforma_variations_mapper = {
+        r"(?i)^chat[-_\s]?gpt$": "ChatGPT",
+        r"(?i)^gemini$": "Gemini",     
+        r"(?i)^perplexity$": "Perplexity"
+    }
+
+    # List treatment
+    if isinstance(raw_data, list):
+        clean_data = []
+        for data in raw_data:
+            if data['id'] not in [d['id'] for d in clean_data]: # Check if exists double-assigned id
+
+                if data['resposta_texto']: # Check if resposta exists
+
+                # Platform treatment                    
+                    for pattern, correct_name in plataforma_variations_mapper.items():
+                        if re.match(pattern, data['plataforma']):
+                            data['plataforma'] = correct_name
+
+                # Datetime treatment
+                    try:
+                        data['data_hora'] = parser.parse(data['data_hora']).strftime(date_pattern)
+                        clean_data.append(data)
+                    except (ValueError, TypeError):
+                        # How datetime will not be used in analysis, I must prefer maintain the data and set datetime.now() as default
+                        data['data_hora'] = datetime.now().strftime(date_pattern)
+                        clean_data.append(data)
+
+        return clean_data
+
+    # Single Dict treatment
+    elif isinstance(raw_data, dict):
+        clean_data = None
+        if raw_data['resposta_texto']: # Check if resposta exists
+
+            # Platform treatment
+            for pattern, correct_name in plataforma_variations_mapper.items():
+                if re.match(pattern, raw_data['plataforma']):
+                    raw_data['plataforma'] = correct_name
+
+            # Datetime treatment
+            try:
+                raw_data['data_hora'] = parser.parse(raw_data['data_hora']).strftime(date_pattern)
+                clean_data = raw_data
+            except (ValueError, TypeError):
+                # How datetime will not be used in analysis, I must prefer maintain the data and set datetime.now() as default
+                raw_data['data_hora'] = datetime.now().strftime(date_pattern) 
+                clean_data = raw_data
+
+            return clean_data
+
+    else:
+        raise TypeError('São suportados apenas listas de dicionários e dicionários. Favor informar um tipo válido.')
+
+
+if __name__ == '__main__':
+    data = json_consumer(JSON_PATH)
+
+    for d in data_cleansing(data):
+        print(d['id'], d['data_hora'], d['plataforma'], d['resposta_texto'] is not None)
